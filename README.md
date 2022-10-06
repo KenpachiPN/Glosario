cfc8c406c612a3efa5369190f2d9ae3010c35fb1
KenpachiPN <sprada71@gmail.com>
2022-10-05 20:49:15 -0500

    Código limpia

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# **** Ciclo ****
+# **** Función limpia ****
 
-Un ciclo es una secuencia de código que se ejecuta varias veces, como les mencionaba anteriormente, esto va de la mano con la iteración, finaliza cuando la condición no se cumpla más.
+Esta consiste en facilitar la escritura y lectura del código, esto para su fácil entendimiento.
 
--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 
-let suma = 0;
-let num = 2;
 for(let i = 1;  i <= 5; i++) {
-    let nom = prompt("Ingrese su nombre");
-    let sueldo = Number(prompt(nom + " ,ingrese su sueldo"));
+    let nombre = prompt("Ingrese su nombre");
+    let sueldo = Number(prompt(nombre + " ,ingrese su sueldo"));
     if(sueldo === 0 ){
         break;
     }
     alert("El sueldo de Santiago es de: $" +sueldo);
-
 }
 
 
